348fba9d5b85ec140c95ef1352a285a22f7d52e2
Andrew Tec <21319730+tecandrew@users.noreply.github.com>
2025-01-31 09:55:36 -0800

    fix: adjust svg to remove width and height, and fix to 0 0 24 24

--- src/generate.sh
+++ src/generate.sh
@@ -6,3 +6,17 @@ for dir in frappe latte macchiato mocha; do
 done
 rm -rf "./assets"
 cp -r "src/vscode-icons/assets" "./assets"
+
+for theme in frappe latte macchiato mocha; do
+    for file in icons/$theme/*.svg; do
+        sed -i '' \
+            -e 's/width="[^"]*"//g' \
+            -e 's/height="[^"]*"//g' \
+            -e 's/viewBox="[^"]*"//g' \
+            -e 's/<svg /<svg viewBox="0 0 24 24" /' \
+            -e 's/[[:space:]]\{2,\}/ /g' \
+            -e 's/ >/>/g' \
+            "$file"
+        echo "Processed $file"
+    done
+done
