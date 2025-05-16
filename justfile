165f6d1962146503edceacc8d97904673d6152b6
Andrew <21319730+tecandrew@users.noreply.github.com>
2025-05-15 22:19:35 -0700

    refactor: only use typescript for theme generation (#31)
    * feat: use typescript to generate icon theme
    
    * refactor: regenerate icon theme. remove old tera + python build
    
    * docs(README): update development info
    
    * chore: default justfile to help menu
    
    * refactor: use id of flavorEntries
    
    * refacor: better use of `.map()`
    
    * chore: cleanup justfile
    
    * Merge branch 'main' into refactor/ts
    
    * feat: regenerate theme and update icons

--- justfile
+++ justfile
@@ -12,8 +12,6 @@ alias init := setup
 [working-directory: 'src']
 @build:
     deno task run
-    whiskers zed-icons.tera
-    echo "-- OK - Built 'icon_themes/catppuccin-icons.json'"
 
 alias publish := deploy
 [doc('git push tag to trigger PR request to zed-industries/extensions')]
@@ -23,12 +21,3 @@ alias publish := deploy
     git tag -s {{ tag }} -m {{ tag }}
     git push origin {{ tag }}
     gh release create {{ tag }} --generate-notes --draft
-
-[group('github')]
-@act event_name="" +args="":
-    act -P ubuntu-24.04-arm=catthehacker/ubuntu:act-latest {{ event_name }} -s GITHUB_TOKEN="$(gh auth token)" {{ args }}
-
-alias aj := act-job
-[group('github')]
-@act-job job_name:
-    act -P ubuntu-24.04-arm=catthehacker/ubuntu:act-latest -j {{ job_name }} -s GITHUB_TOKEN="$(gh auth token)"
---
+++ src/build.ts
@@ -0,0 +1,98 @@
+import { flavorEntries, version } from "@catppuccin/palette";
+import { fileIcons } from "./vscode-icons/src/defaults/fileIcons.ts";
+
+console.log("-- ctp/palette v" + version);
+
+const ZED_THEME = {
+  "$schema": "https://zed.dev/schema/icon_themes/v0.2.0.json",
+  "name": "Catppuccin Icons",
+  "author": "Catppuccin <releases@catppuccin.com>",
+  "themes": []
+};
+
+const THEME_OVERRIDES = {
+  "chevron_icons": {
+    "collapsed": null,
+    "expanded": null
+  },
+  "file_stems": {
+    "LICENSE": "license",
+    "README": "readme",
+    "Makefile": "makefile",
+    "Caddyfile": "caddy",
+    "Dockerfile": "docker"
+  }
+}
+
+// stage 1: create `file_suffixes` for zed schema
+// example: `{ "aep": "adobe-ae" }`
+let suffixEntries = {};
+suffixEntries["file_suffixes"] = {};
+for (const [vsCodeFileIcons_key, vscodeFileIcons_value] of Object.entries(fileIcons)) {
+
+  vscodeFileIcons_value.fileNames?.forEach(data => {
+    if (!suffixEntries.hasOwnProperty(data)) {
+      suffixEntries["file_suffixes"][data] = vsCodeFileIcons_key;
+    } else {
+      console.warn(`-- Key '${vsCodeFileIcons_key}' already exists, appending anyway...`);
+      suffixEntries["file_suffixes"][data] = vsCodeFileIcons_key;
+    }
+  });
+
+  vscodeFileIcons_value.fileExtensions?.forEach(data => {
+    if (!suffixEntries.hasOwnProperty(data)) {
+      suffixEntries["file_suffixes"][data] = vsCodeFileIcons_key;
+    } else {
+      console.warn(`-- Key '${vsCodeFileIcons_key}' already exists, appending anyway...`);
+      suffixEntries["file_suffixes"][data] = vsCodeFileIcons_key;
+    }
+  });
+}
+
+// stage 2: compile flavor specific properties
+let THEME = {};
+const fileIconEntries = {};
+fileIconEntries["file_icons"] = {};
+const flavorProperties: { [key: string]: any } = {};
+
+function createDirectoryIcons(flvr: string) {
+  return {
+    directory_icons: {
+      collapsed: `./icons/${flvr}/_folder.svg`,
+      expanded: `./icons/${flvr}/_folder_open.svg`
+    }
+  };
+}
+
+const themes = flavorEntries.map(([id, flavor]) => {
+
+  flavorProperties["name"] = `Catppuccin ${flavor.name}`
+  flavorProperties["appearance"] = `${flavor.dark ? "dark" : "light"}`;
+  const directoryIconsEntries = createDirectoryIcons(id);
+
+  for (const [vsCodeFileIcons_key, _] of Object.entries(fileIcons)) {
+
+    if (!fileIconEntries.hasOwnProperty(vsCodeFileIcons_key)) {
+      fileIconEntries["file_icons"][vsCodeFileIcons_key] = { "path": `./icons/${id}/${vsCodeFileIcons_key}.svg` };
+    } else {
+      // console.warn(`-- Key '${"file_icons"}' already exists, appending anyway...`);
+      fileIconEntries["file_icons"][vsCodeFileIcons_key] = { "path": `./icons/${id}/${vsCodeFileIcons_key}.svg` };
+    }
+  }
+
+  return { ...flavorProperties, ...directoryIconsEntries, ...THEME_OVERRIDES, ...suffixEntries, ...fileIconEntries };
+});
+
+ZED_THEME.themes.push(...themes);
+
+// stage 3: create final theme
+const ZED_THEME_JSON = JSON.stringify(ZED_THEME, null, 2);
+const ZED_THEME_FILENAME = "../icon_themes/catppuccin-icons.json";
+console.log(ZED_THEME_JSON);
+
+try {
+  await Deno.writeTextFile(ZED_THEME_FILENAME, ZED_THEME_JSON);
+  console.log(`Data successfully saved to ${ZED_THEME_FILENAME}`);
+} catch (error) {
+  console.error("Error writing to file:", error);
+}
