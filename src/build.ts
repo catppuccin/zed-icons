64cc7c15ec2e35b084deb32699bc6da905c07f1d
Andrew Tec <21319730+tecandrew@users.noreply.github.com>
2025-09-13 12:14:10 -0700

    chore: minor adjustments for linting

--- src/build.ts
+++ src/build.ts
@@ -4,46 +4,51 @@ import { fileIcons } from "./vscode-icons/src/defaults/fileIcons.ts";
 console.log("-- ctp/palette v" + version);
 
 const ZED_THEME = {
-  "$schema": "https://zed.dev/schema/icon_themes/v0.2.0.json",
-  "name": "Catppuccin Icons",
-  "author": "Catppuccin <releases@catppuccin.com>",
-  "themes": []
+  $schema: "https://zed.dev/schema/icon_themes/v0.2.0.json",
+  name: "Catppuccin Icons",
+  author: "Catppuccin <releases@catppuccin.com>",
+  themes: [],
 };
 
 const THEME_OVERRIDES = {
-  "chevron_icons": {
-    "collapsed": null,
-    "expanded": null
+  chevron_icons: {
+    collapsed: null,
+    expanded: null,
   },
-  "file_stems": {
-    "LICENSE": "license",
-    "README": "readme",
-    "Makefile": "makefile",
-    "Caddyfile": "caddy",
-    "Dockerfile": "docker"
-  }
-}
+  file_stems: {
+    LICENSE: "license",
+    README: "readme",
+    Makefile: "makefile",
+    Caddyfile: "caddy",
+    Dockerfile: "docker",
+  },
+};
 
 // stage 1: create `file_suffixes` for zed schema
 // example: `{ "aep": "adobe-ae" }`
 let suffixEntries = {};
 suffixEntries["file_suffixes"] = {};
-for (const [vsCodeFileIcons_key, vscodeFileIcons_value] of Object.entries(fileIcons)) {
-
-  vscodeFileIcons_value.fileNames?.forEach(data => {
+for (const [vsCodeFileIcons_key, vscodeFileIcons_value] of Object.entries(
+  fileIcons,
+)) {
+  vscodeFileIcons_value.fileNames?.forEach((data) => {
     if (!suffixEntries.hasOwnProperty(data)) {
       suffixEntries["file_suffixes"][data] = vsCodeFileIcons_key;
     } else {
-      console.warn(`-- Key '${vsCodeFileIcons_key}' already exists, appending anyway...`);
+      console.warn(
+        `-- Key '${vsCodeFileIcons_key}' already exists, appending anyway...`,
+      );
       suffixEntries["file_suffixes"][data] = vsCodeFileIcons_key;
     }
   });
 
-  vscodeFileIcons_value.fileExtensions?.forEach(data => {
+  vscodeFileIcons_value.fileExtensions?.forEach((data) => {
     if (!suffixEntries.hasOwnProperty(data)) {
       suffixEntries["file_suffixes"][data] = vsCodeFileIcons_key;
     } else {
-      console.warn(`-- Key '${vsCodeFileIcons_key}' already exists, appending anyway...`);
+      console.warn(
+        `-- Key '${vsCodeFileIcons_key}' already exists, appending anyway...`,
+      );
       suffixEntries["file_suffixes"][data] = vsCodeFileIcons_key;
     }
   });
@@ -51,41 +56,53 @@ for (const [vsCodeFileIcons_key, vscodeFileIcons_value] of Object.entries(fileIc
 
 // stage 2: compile flavor specific properties
 const FILE_ICON_OVERRIDES = {
-  "lock": "lock",
-  "settings": "config"
-}
+  lock: "lock",
+  settings: "config",
+};
 
 function createDirectoryIcons(flvr: string) {
   return {
     directory_icons: {
       collapsed: `./icons/${flvr}/_folder.svg`,
-      expanded: `./icons/${flvr}/_folder_open.svg`
-    }
+      expanded: `./icons/${flvr}/_folder_open.svg`,
+    },
   };
 }
 
 const themes = flavorEntries.map(([id, flavor]) => {
-  const localFileIconEntries: { file_icons: { [key: string]: { path: string } } } = { "file_icons": {} };
+  const localFileIconEntries: {
+    file_icons: { [key: string]: { path: string } };
+  } = { file_icons: {} };
 
   // Theme-specific properties
   const currentFlavorProperties = {
-    "name": `Catppuccin ${flavor.name}`,
-    "appearance": flavor.dark ? "dark" : "light"
+    name: `Catppuccin ${flavor.name}`,
+    appearance: flavor.dark ? "dark" : "light",
   };
 
   const directoryIconsEntries = createDirectoryIcons(id);
 
   // Populate from vscode-icons defaults first
   for (const [vsCodeFileIcons_key, _] of Object.entries(fileIcons)) {
-    localFileIconEntries["file_icons"][vsCodeFileIcons_key] = { "path": `./icons/${id}/${vsCodeFileIcons_key}.svg` };
+    localFileIconEntries["file_icons"][vsCodeFileIcons_key] = {
+      path: `./icons/${id}/${vsCodeFileIcons_key}.svg`,
+    };
   }
 
   // Apply FILE_ICON_OVERRIDES
   for (const [overrideKey, iconName] of Object.entries(FILE_ICON_OVERRIDES)) {
-    localFileIconEntries["file_icons"][overrideKey] = { "path": `./icons/${id}/${iconName}.svg` };
+    localFileIconEntries["file_icons"][overrideKey] = {
+      path: `./icons/${id}/${iconName}.svg`,
+    };
   }
 
-  return { ...currentFlavorProperties, ...directoryIconsEntries, ...THEME_OVERRIDES, ...suffixEntries, ...localFileIconEntries };
+  return {
+    ...currentFlavorProperties,
+    ...directoryIconsEntries,
+    ...THEME_OVERRIDES,
+    ...suffixEntries,
+    ...localFileIconEntries,
+  };
 });
 
 ZED_THEME.themes.push(...themes);
